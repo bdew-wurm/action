792ebbb63a101aca5abca7aae3417bb40999d82a
Jonathan Walker <xorith@gmail.com>
2016-03-20 13:49:52 -0400

    Added 'selected' as action target for items in the Select Bar.

--- src/main/java/net/bdew/wurm/action/ActionMod.java
+++ src/main/java/net/bdew/wurm/action/ActionMod.java
@@ -1,6 +1,7 @@
 package net.bdew.wurm.action;
 
 import com.wurmonline.client.console.WurmConsole;
+import com.wurmonline.client.renderer.PickableUnit;
 import com.wurmonline.client.renderer.gui.HeadsUpDisplay;
 import com.wurmonline.shared.constants.PlayerAction;
 import javassist.ClassPool;
@@ -60,6 +61,15 @@ public class ActionMod implements WurmMod, Initable, PreInitable {
                     return true;
                 } else if (data[2].equals("tile")) {
                     hud.getWorld().sendLocalAction(new PlayerAction(id, PlayerAction.ANYTHING));
+                    return true;
+                } else if (data[2].equals("selected")) {
+                    try {
+                        PickableUnit p = Reflect.getSelectedUnit(hud.getSelectBar());
+                        if(p != null)
+                            hud.sendAction(new PlayerAction(id, PlayerAction.ANYTHING), p.getId());
+                    } catch (ReflectiveOperationException e) {
+                        throw new RuntimeException(e);
+                    }
                     return true;
                 }
             }
--- src/main/java/net/bdew/wurm/action/Reflect.java
+++ src/main/java/net/bdew/wurm/action/Reflect.java
@@ -1,9 +1,11 @@
 package net.bdew.wurm.action;
 
 import com.wurmonline.client.game.inventory.InventoryMetaItem;
+import com.wurmonline.client.renderer.PickableUnit;
 import com.wurmonline.client.renderer.gui.HeadsUpDisplay;
 import com.wurmonline.client.renderer.gui.PaperDollInventory;
 import com.wurmonline.client.renderer.gui.PaperDollSlot;
+import com.wurmonline.client.renderer.gui.SelectBar;
 
 import java.lang.reflect.Field;
 
@@ -14,6 +16,9 @@ public class Reflect {
 
     // PaperDollInventory
     static private Field fldBodyItem;
+    
+    // Selected Unit
+    static private Field fldSelectedUnit;
 
 
     static public void setup() throws ReflectiveOperationException {
@@ -22,6 +27,9 @@ public class Reflect {
 
         fldBodyItem = PaperDollInventory.class.getDeclaredField("bodyItem");
         fldBodyItem.setAccessible(true);
+        
+        fldSelectedUnit = SelectBar.class.getDeclaredField("selectedUnit");
+        fldSelectedUnit.setAccessible(true);
     }
 
     public static PaperDollInventory getPaperdollInventory(HeadsUpDisplay hud) throws ReflectiveOperationException {
@@ -30,5 +38,9 @@ public class Reflect {
 
     public static InventoryMetaItem getBodyItem(PaperDollInventory pd) throws ReflectiveOperationException {
         return ((PaperDollSlot) fldBodyItem.get(pd)).getItem();
+    }
+    
+    public static PickableUnit getSelectedUnit(SelectBar s) throws ReflectiveOperationException {
+        return (PickableUnit) fldSelectedUnit.get(s);
     }
 }
